5f1822572dd994bc1e7d6dd2cb52e4715f0f9fe0
Nitesh <niteshmali132002@gmail.com>
2023-01-01 01:38:30 +0530

    Initial Commit

---
+++ AI.py
@@ -0,0 +1,103 @@
+import openai
+import os
+import pyttsx3
+import urllib.request
+import speech_recognition as sr
+from playsound import playsound
+from dotenv import load_dotenv
+
+load_dotenv()
+
+openai.api_key = os.getenv("OPENAIAPI")
+internet = False
+
+
+
+def ask_question(question):
+    response = openai.Completion.create(
+        engine="text-davinci-002",
+        prompt=question,
+        max_tokens=1024,
+        n=1,
+        stop=None,
+        temperature=0.5
+    )
+    return response.choices[0].text
+
+
+def speak(text):
+    engine = pyttsx3.init()
+    rate = engine.getProperty('rate')
+    engine.setProperty('rate', rate-50)
+    voices = engine.getProperty('voices')
+    engine.setProperty('voice', voices[1].id)
+    engine.say(text)
+    engine.runAndWait()
+
+
+def checkInternet():
+    url = "http://www.google.com"
+    try:
+        urllib.request.urlopen(url)
+        return True
+    except urllib.error.URLError:
+        speak("By the way, before you ask any questions, I see that you are not connected to the internet or there is a connection problem.")
+        speak("I cannot answer any of your questions without internet. So please try again, after ensuring strong internet connection.")
+        playsound("end.mp3")
+        return False
+
+
+def startAssistant():
+    while internet == True:
+        r = None
+        r = sr.Recognizer()
+        r.pause_threshold = 1
+        r.energy_threshold = 300
+        with sr.Microphone() as source:
+            playsound("start.mp3")
+            print("Listening...")
+            audio = r.listen(source)
+
+        continueQuestion = True
+        try:
+            print("Recognizing...")
+            question = r.recognize_google(audio)
+            print("\nYou: "+question)
+            if question == ("no" or "NO" or "No"):
+                continueQuestion = False
+        except sr.UnknownValueError:
+            speak("No audio detected!")
+            continueQuestion = False
+        except sr.RequestError as e:
+            speak("Could not request results from Google Speech Recognition service")
+            continueQuestion = False
+
+        if continueQuestion == True:
+            response = ask_question(question)
+            print("\nFriday: "+response)
+            speak(str(response))
+            r = None
+        else:
+            playsound("end.mp3")
+            break
+
+
+playsound("start.mp3")
+speak("Hello, I am Friday, you can ask me any questions if you want, say no to quit the program.")
+# while True:
+#     r = sr.Recognizer()
+#     r.pause_threshold = 1
+#     r.energy_threshold = 300
+#     with sr.Microphone() as source:
+#         phrase = r.listen(source)
+#     try:
+#         text = r.recognize_google(phrase)
+#         r = None
+#         if text.lower() == ("ok robin" or "Ok Robin" or "ok Robin"):
+#             print("Input Received Successfully!\r")
+internet = checkInternet()
+startAssistant()
+r = None
+    # except sr.UnknownValueError:
+    #     print("Retrying!")
+    #     continue
---
+++ README.md
@@ -0,0 +1,23 @@
+# Python program to demonstrate openai API
+
+OpenAI is an artificial intelligence (AI) research laboratory. This Project is a demonstration of its API for the "davinci model 002". This program requests any question or query to the model via api and converts the response text into speech.
+
+## Installation
+
+Use the package manager [pip](https://pip.pypa.io/en/stable/) to install openai module.
+
+```bash
+pip install openai
+```
+
+
+## Contributing
+
+Pull requests are welcome. For major changes, please open an issue first
+to discuss what you would like to change.
+
+Please make sure to update tests as appropriate.
+
+## License
+
+[MIT](https://choosealicense.com/licenses/mit/)
